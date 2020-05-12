25bea5b70b40154f991a28db4850e4e4f599e01a
sinsynplus <plus@sinsynplus.de>
2020-05-12 03:21:33 +0200

    Add files via upload

--- src/ofxTriangleMesh.cpp
+++ src/ofxTriangleMesh.cpp
@@ -1,30 +1,20 @@
 #include "ofxTriangleMesh.h"
 #include "triangle.h"
 
-
-
-
-
-
-
-void triangulatePoints(char * flags, triangulateio * in, triangulateio * mid, 
-                       triangulateio * out){
-
+void triangulatePoints(char * flags, triangulateio * in, triangulateio * mid, triangulateio * out){
     // this funciton, which calls triangulage is because we have a function called triangulate, so the compiler get's a bit confused.
     triangulate(flags, in,  mid, out);
 }
-
 
 ofxTriangleMesh::ofxTriangleMesh(){
     nTriangles = 0;
 }
 
-
 // see note in the h file for how to use the parameters here....
 void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, float sizeConstraint){
-
+    
     int bSize = contour.size();
-   
+    
     struct triangulateio in, out;
     in.numberofpoints = bSize;
     in.numberofpointattributes = 0;
@@ -37,7 +27,7 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
 		in.pointlist[i*2+0] = contour[i].x;
         in.pointlist[i*2+1] = contour[i].y;
     }
-	
+    
     out.pointlist = (REAL *) NULL;            
     out.pointattributelist = (REAL *) NULL;
     out.pointmarkerlist = (int *) NULL; 
@@ -47,16 +37,13 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
     out.segmentlist = (int *) NULL;
     out.segmentmarkerlist = (int *) NULL;
     out.edgelist = (int *) NULL;
-    out.edgemarkerlist = (int *) NULL; 
- 
+    out.edgemarkerlist = (int *) NULL;
     
     bool bConstrainAngle = false;
     bool bConstrainSize = false;
     
     if (angleConstraint > 0) bConstrainAngle = true;
     if (sizeConstraint > 0) bConstrainSize = true;
-    
-    
     
     string triangulateParams;
     
@@ -72,9 +59,7 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
         triangulateParams += "a" + ofToString( (int)sizeConstraint );
     }
     
-    
     triangulatePoints((char *) triangulateParams.c_str(), &in, &out, NULL);
-
     
     /*
     printf("Initial triangulation:\n\n");
@@ -89,12 +74,10 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
         }
     */
     
-    
     nTriangles = 0;
     triangles.clear();
     
-    
-    std::map < int , ofPoint  > goodPts;
+    std::map < int, glm::vec3 > goodPts;
     
     for (int i = 0; i < out.numberoftriangles; i++) {
         meshTriangle triangle;
@@ -103,21 +86,16 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
         
         for (int j = 0; j < 3; j++){
             whichPt = out.trianglelist[i * 3 + j];
-            triangle.pts[j] = ofPoint(  out.pointlist[ whichPt * 2 + 0],  out.pointlist[ whichPt * 2 + 1]);
+            triangle.pts[j] = glm::vec3(out.pointlist[whichPt * 2 + 0], out.pointlist[whichPt * 2 + 1], 0);
             triangle.index[j] = whichPt;
-            
-            
-            
         }
         
-        ofPoint tr[3];
+        glm::vec3 tr[3];
         tr[0] = triangle.pts[0];
         tr[1] = triangle.pts[1];
         tr[2] = triangle.pts[2];
-		
         
         // here we check if a triangle is "inside" a contour to drop non inner triangles
-        
         if( isPointInsidePolygon(&contour[0], contour.size(), getTriangleCenter(tr) ) ) {
             triangle.randomColor = ofColor(ofRandom(0,255), ofRandom(0,255), ofRandom(0,255));
             triangles.push_back(triangle);
@@ -133,26 +111,26 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
     // put all good points in a vector and handle the remapping of indices.
     // the indices stored above were for all points, but since we drop triangles, we 
     // can drop non used points, and then remap all the indces. 
-    // that happens here: 
+    // that happens here:
     
     outputPts.clear();
     std::map < int, int > indexChanges;
-    std::map< int , ofPoint >::iterator iter;
+    std::map < int, glm::vec3 >::iterator iter;
     for (iter = goodPts.begin(); iter != goodPts.end(); ++iter) {
         //cout << iter->first << " " << iter->second << endl;
-        indexChanges[iter->first] = outputPts.size();
+        int n = outputPts.size();
+        indexChanges[iter->first] = n;
         outputPts.push_back(iter->second);
     }
     
-    // now, with the new, potentially smaller group of points, update all the indices of the triangles so their indices point right: 
-    
+    // now, with the new, potentially smaller group of points, update all the indices of the triangles so their indices point right:
     for (int i = 0; i < triangles.size(); i++){
         for (int j = 0; j < 3; j++){
             triangles[i].index[j] = indexChanges[triangles[i].index[j]];
         }
     }
     
-    // now make a mesh, using indices: 
+    // now make a mesh, using indices:
     
     triangulatedMesh.clear();
     triangulatedMesh.setMode(OF_PRIMITIVE_TRIANGLES);
@@ -165,10 +143,10 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
         triangulatedMesh.addIndex(triangles[i].index[1]);;
         triangulatedMesh.addIndex(triangles[i].index[2]);;
     }
-
+    
     // depending on flags, we may need to adjust some of the memory clearing
     // (see tricall.c for full listings)
-    // TODO: this should be agressively tested. 
+    // TODO: this should be agressively tested.
     
     free(in.pointlist);
     free(out.pointlist);
@@ -178,7 +156,6 @@ void ofxTriangleMesh::triangulate(ofPolyline contour, float angleConstraint, flo
     if (out.triangleattributelist != NULL) free(out.triangleattributelist);
     
     return;
-
 }
 
 void ofxTriangleMesh::clear(){
@@ -186,23 +163,21 @@ void ofxTriangleMesh::clear(){
     nTriangles = 0;
 }
 
-ofPoint ofxTriangleMesh::getTriangleCenter(ofPoint *tr){
+glm::vec3 ofxTriangleMesh::getTriangleCenter(glm::vec3 *tr){
     float c_x = (tr[0].x + tr[1].x + tr[2].x) / 3;
     float c_y = (tr[0].y + tr[1].y + tr[2].y) / 3;
-    return ofPoint(c_x, c_y);
+    return glm::vec3(c_x, c_y, 0);
 }
 
-bool ofxTriangleMesh::isPointInsidePolygon(ofPoint *polygon,int N, ofPoint p)
-{
+bool ofxTriangleMesh::isPointInsidePolygon(glm::vec3 *polygon,int N, glm::vec3 p){
     int counter = 0;
     int i;
     double xinters;
-    ofPoint p1,p2;
-
+    glm::vec3 p1,p2;
+    
     p1 = polygon[0];
-
-    for (i=1;i<=N;i++)
-    {
+    
+    for (i=1;i<=N;i++){
         p2 = polygon[i % N];
         if (p.y > MIN(p1.y,p2.y)) {
             if (p.y <= MAX(p1.y,p2.y)) {
@@ -221,24 +196,15 @@ bool ofxTriangleMesh::isPointInsidePolygon(ofPoint *polygon,int N, ofPoint p)
 	return counter % 2 != 0;
 }
 
-
-void ofxTriangleMesh::draw() {
-
-    // draw the triangles in their random colors: 
-    
+void ofxTriangleMesh::draw(){
+    // draw the triangles in their random colors:
     for (int i=0; i<nTriangles; i++){
-    
         ofFill();
         ofSetColor( triangles[i].randomColor);
-        ofTriangle( outputPts[triangles[i].index[0]], 
-        outputPts[triangles[i].index[1]],
-                   outputPts[triangles[i].index[2]]);
-        
+        ofTriangle( outputPts[triangles[i].index[0]], outputPts[triangles[i].index[1]], outputPts[triangles[i].index[2]]);
     }
     
-    // draw the mesh as a wire frame in white on top. 
-    
+    // draw the mesh as a wire frame in white on top.
     ofSetColor(255,255,255);
     triangulatedMesh.drawWireframe();
-    
 }
--- src/ofxTriangleMesh.h
+++ src/ofxTriangleMesh.h
@@ -2,13 +2,10 @@
  
  ofxTriangleMesh by zach lieberman
  
- based off of 
-
  ofxTriangle by kikko.fr
  Point inside polygon by Theo / paul bourke
  
-
- this code uses the original triangle (not triangle++) to allow for switches like 
+ this code uses the original triangle (not triangle++) to allow for switches like
  q33 a100, etc 
  
  https://www.cs.cmu.edu/~quake/triangle.switch.html
@@ -21,62 +18,46 @@
 
 #pragma once
 
-
-
 #include "ofMain.h"
-
-
 
 
 typedef struct{
     
-    ofPoint pts[3];
+    glm::vec3 pts[3];
     int index[3];           // for the mesh, what points does this triangle relate to.
-    ofColor randomColor;    // useful for debugging / drawing 
-
+    ofColor randomColor;    // useful for debugging / drawing
+    
 } meshTriangle;
 
 
-
-
-
 class ofxTriangleMesh {
-    
-    public :
 
+public:
     
+    ofxTriangleMesh();
     
-        ofxTriangleMesh();
-        
+    // usage notes:
     
-        // usage notes: 
+    // -1 = don't use constraint, other values = use constraint
+    //
+    // for angle, 20-30 is pretty good
+    // https://www.cs.cmu.edu/~quake/triangle.q.html
+    // be careful!  "It usually doesn't terminate for angles above 34o"
+    //
+    // for size, this depends on the size of your shape,
+    // 100 to 200 is a good first guess for screen resolution based points
     
-        // -1 = don't use constraint, other values = use constraint 
-        // 
-        // for angle, 20-30 is pretty good
-        // https://www.cs.cmu.edu/~quake/triangle.q.html
-        // be careful!  "It usually doesn't terminate for angles above 34o"
-        //
-        // for size, this depends on the size of your shape, 
-        // 100 to 200 is a good first guess for screen resolution based points
+    void triangulate(ofPolyline contour, float angleConstraint = -1, float sizeConstraint = -1);
     
-        void triangulate(ofPolyline contour, float angleConstraint = -1, float sizeConstraint = -1);
-
+    glm::vec3 getTriangleCenter(glm::vec3 *tr);
+    bool isPointInsidePolygon(glm::vec3 *polygon,int N, glm::vec3 p);
     
-        
-        ofPoint getTriangleCenter(ofPoint *tr);
-        bool isPointInsidePolygon(ofPoint *polygon,int N, ofPoint p);
-
-        void draw();
-        void clear();
-
-        int nTriangles;
-        vector <ofPoint> outputPts;
-        vector <meshTriangle> triangles;
-        ofMesh triangulatedMesh;
+    void draw();
+    void clear();
     
-      
+    int nTriangles;
+    vector <glm::vec3> outputPts;
+    vector <meshTriangle> triangles;
+    ofMesh triangulatedMesh;
     
-
-
 };
